0af0caf55a39a0f8f80d45125c949dc6feb1cf55
Brage Skjønborg <bskjon@outlook.com>
2025-07-24 01:21:34 +0200

    Changes

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,1 @@
-self.firebaseConfig = {
-  "apiKey": "AIzaSyC48XmLSon4WN43dyE0d6g3Z3TascUOZp0",
-  "authDomain": "streamit-f06d7.firebaseapp.com",
-  "databaseURL": "https://streamit-f06d7.firebaseio.com",
-  "projectId": "streamit-f06d7",
-  "storageBucket": "streamit-f06d7.firebasestorage.app",
-  "messagingSenderId": "53644543109",
-  "appId": "1:53644543109:web:10c2e1329f13487f28d5ec",
-  "measurementId": "G-0V2MNL1D2J"
-};
+self.firebaseConfig = {};
